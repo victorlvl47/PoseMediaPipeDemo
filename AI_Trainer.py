c77987d95407ce95da0e10d28640bdd512d89811
victor <victor.lvl47@gmail.com>
2021-08-28 16:01:40 -0600

    findAngle function defined

--- AI_Trainer.py
+++ AI_Trainer.py
@@ -1,14 +1,23 @@
 import cv2
 import numpy as np
 import time
+import PoseModule as pm
 
 cap = cv2.VideoCapture("PoseVideos/7.mp4")
-img = cv2.imread("PoseImages/workout1.jpg")
-img = cv2.resize(img, (480, 640))
+detector = pm.poseDetector()
 
 while True:
     # success, img = cap.read()
     # img = cv2.resize(img, (640, 480))
+
+    img = cv2.imread("PoseImages/workout1.jpg")
+    img = cv2.resize(img, (480, 640))
+
+    img = detector.findPose(img, False)
+
+    lmList = detector.findPosition(img, False)
+    if len(lmList) != 0:
+        detector.findAngle(img, 12, 14, 16)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
--- PoseModule.py
+++ PoseModule.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import time
+import math
 
 class poseDetector():
 
@@ -48,6 +49,33 @@ class poseDetector():
                     cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
 
         return self.lmList
+
+    def findAngle(self, img, p1, p2, p3, draw=True):
+
+        # landmarks
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        x3, y3 = self.lmList[p3][1:]
+
+        # calculate the angle
+        angle  = math.degrees(math.atan2(y3 - y2, x3 - x2) - 
+            math.atan2(y1 - y2, x1 - x2))
+        print(angle)
+
+
+        if draw:
+            # lines
+            cv2.line(img, (x1, y1), (x2, y2), (6, 84, 230), 3)
+            cv2.line(img, (x3, y3), (x2, y2), (6, 84, 230), 3)
+            # circles
+            # rgb(11, 230, 236)
+            cv2.circle(img, (x1, y1), 5, (236, 230, 11), cv2.FILLED)
+            cv2.circle(img, (x1, y1), 15, (236, 230, 11), 2)
+            cv2.circle(img, (x2, y2), 5, (236, 230, 11), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (236, 230, 11), 2)
+            cv2.circle(img, (x3, y3), 5, (236, 230, 11), cv2.FILLED)
+            cv2.circle(img, (x3, y3), 15, (236, 230, 11), 2)
+
 
 
 
